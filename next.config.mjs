76b0afc8afefff2466a3b15ee54b9764562cc222
AlearvickOne <alexandr73.73.73.73@mail.ru>
2024-05-13 22:17:46 +0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
     config.resolve.alias["mobx-react"] = "mobx-react-lite";
     return config;
   },
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_YANDEX_MAPS_API: process.env.NEXT_PUBLIC_YANDEX_MAPS_API,
+    NEXT_PUBLIC_YANDEX_MAPS_GEO_API: process.env.NEXT_PUBLIC_YANDEX_MAPS_GEO_API,
+  },
 };
 
 export default nextConfig;
